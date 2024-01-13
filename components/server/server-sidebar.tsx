5a5dcf6523bd5c8075c11a11f5ab3e6602611ce3
PranotiPD <pranotidurugkar1111@gmail.com>
2024-01-13 14:38:25 +0530

    feat: invitation modal

--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -1,6 +1,8 @@
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
+import { ChannelType } from "@prisma/client";
 import { redirect } from "next/navigation";
+import ServerHeader from "./server-header";
 
 interface ServerSidebarProps {
     serverId: string
@@ -8,7 +10,7 @@ interface ServerSidebarProps {
 const ServerSidebar = async ({
     serverId
 }:ServerSidebarProps) => {
-    const profile = currentProfile();
+    const profile = await currentProfile();
 
     if(!profile){
         return redirect("/");
@@ -23,13 +25,35 @@ const ServerSidebar = async ({
                 orderBy: {
                     createdAt: "asc"
                 }
+            },
+            members: {
+                include: {
+                    profile: true
+                },
+                orderBy: {
+                    role: "asc"
+                }
             }
-        }
+        },
     })
 
+    const textChannels = server?.channels.filter((channel) => channel.type === ChannelType.TEXT)
+    const audioChannels = server?.channels.filter((channel) => channel.type === ChannelType.AUDIO)
+    const videoChannels = server?.channels.filter((channel) => channel.type === ChannelType.VIDEO)
+
+    const members = server?.members.filter((member) => member.profileId !== profile.id)
+
+    const role = server?.members.find((member) => member.profileId === profile.id)?.role
+
+    if(!server){
+        return redirect("/");
+    }
     return ( 
-        <div>
-            server sidebar component
+        <div className="flex flex-col h-full text-primary w-full dark:bg-[#2B2D31]
+        bg-[#F2F3F5]">
+            <ServerHeader 
+            server={server}
+            role={role}/>
         </div>
      );
 }
